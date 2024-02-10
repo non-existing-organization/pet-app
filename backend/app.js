9f24b5d6d6314dcfe3e52624e52b5b17052168aa
John Stilia <stilia.johny@gmail.com>
2024-02-11 00:46:34 +0100

    feat: Add Express API with Swagger documentation

---
+++ backend/app.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
+const fs = require("fs");
+const path = require("path");
+const bookingServicesRouter = require("./v1/list-booking-services"); // Make sure this path matches your structure
+const responseMiddleware = require("./utils/response_middleware");
+
+const authenticateToken = require('./utils/auth_middleware');
+
+const app = express();
+const port = 3001;
+
+// Swagger definition
+const swaggerDefinition = {
+  openapi: "3.0.0",
+  info: {
+    title: "Express API with Swagger",
+    version: "1.0.0",
+    description: `This is an API for he Pet Sitter App.<br>You can download the OpenAPI specification directly from [/openapi.json](/openapi.json).`,
+  },
+  tags: [
+    {
+      name: "Authentication",
+      description: "Endpoints related to authentication",
+    },
+    {
+      name: "Booking Services",
+      description: "Endpoints related to booking services",
+    },
+    {
+      name: "Users",
+      description: "Endpoints related to users",
+    },
+    {
+      name: "Payments",
+      description: "Endpoints related to payments",
+    }
+  ],
+  servers: [
+    {
+      url: "/api/v1",
+      description: "Version 1",
+    },
+  ],
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+        bearer: "JWT",
+      },
+    },
+  },
+};
+
+// Options for the swagger docs
+const options = {
+  swaggerDefinition,
+  explorer: true,
+  swaggerOptions: {
+    validatorUrl: null,
+  },
+  // Paths to files containing OpenAPI definitions
+  apis: ["./v1/*.js"],
+};
+
+// Initialize swagger-jsdoc -> returns validated swagger spec in json format
+const swaggerSpec = swaggerJsdoc(options);
+
+// Serve Swagger UI
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, options));
+
+// Serve the Swagger specification directly from memory at this route
+app.get("/openapi.json", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(swaggerSpec);
+});
+
+// Protected basic route with authentication middleware
+app.get("/", (req, res) => {
+  // This code will only be reached if the authentication is successful
+  res.send("Hello World!");
+});
+
+
+
+// Apply the response middleware globally
+app.use(responseMiddleware);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  res.status(500).send({ success: false, error: err.message });
+});
+
+// Use versioned routes
+app.use("/api/v1", bookingServicesRouter);
+
+// Start server
+app.listen(port, () => {
+  console.log(`API running at http://localhost:${port}`);
+});
