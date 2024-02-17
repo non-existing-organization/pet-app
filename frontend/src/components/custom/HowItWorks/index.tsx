2ba0a40a242894cd3caeef917df949f66fd99ae7
yezenalnafei <yezen.alnafei@gmail.com>
2024-02-17 19:00:34 +0000

    updated components

--- frontend/src/components/custom/HowItWorks/index.tsx
+++ frontend/src/components/custom/HowItWorks/index.tsx
@@ -1,49 +1,142 @@
 'use client';
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import { FaSearch, FaCalendar, FaCashRegister } from 'react-icons/fa';
-import styles from './component.module.scss';
+import { TbDeviceIpadSearch } from 'react-icons/tb';
+import { FiUserCheck } from 'react-icons/fi';
+import { BsCreditCard2Back } from "react-icons/bs";
+
+import styles from './component.module.css';
 
 function HowItWorks() {
   return (
-    <div>
-      <div>
-      <h2 className="text-center font-weight-bold">Here's how JOLLY works.</h2>
-        <p className="text-center">Search and connect with trusted pet sitters. It's easy as 1, 2, 3.</p>
-      </div>
-      <Container className='pt-4' style={{ minHeight: '50vh' }}>
-        <Row className="justify-content-center">
-          <Col md={4}>
-            <div className="text-centre" style={{ textAlign: 'center' }}>
-              <div className={styles['blue-circle']}>
-                <FaSearch className={styles['icon']} />
+    <section className={`${styles['text-center']} ${styles['pos-r']}`}>
+      <Container>
+        <Row>
+          <Col className="text-center">
+            <div className={styles['section-title']}>
+              <div className={styles['title-effect']}>
+                <div className={styles['bar']}></div>
+                <div className={styles['bar']}></div>
+                <div className={styles['bar']}></div>
+                <div className={styles['bar']}></div>
               </div>
-              <h2>Find a verified pet sitter</h2>
-              <p>Search and find experienced local pet sitters nearby</p>
+              <h6 style={{color: 'grey'}}>How It Work</h6>
+              <h3 className={styles['title']}>Discover and engage with reliable pet caregivers. It's as simple as counting to three!</h3>
             </div>
           </Col>
-          <Col md={4} className="justify-content-center align-items-center">
-            <div className="text-centre" style={{ textAlign: 'center' }}>
-              <div className={styles['blue-circle']}>
-                <FaCalendar className={styles['icon']} />
+        </Row>
+        <Row>
+          <Col lg={4} md={12} className="text-center">
+            <div className={styles['work-process']}>
+              <div className={styles['box-loader']}>
+                <span></span>
+                <span></span>
+                <span></span>
               </div>
-              <h2>Arrange a meet & greet</h2>
-              <p>Get to know your sitter in person, obligation-free.</p>
+              <div className={styles['step-num-box']}>
+                <div className={styles['step-icon']}>
+                  <span>
+                    <TbDeviceIpadSearch />
+                  </span>
+                </div>
+                <div className={styles['step-num']}>01</div>
+              </div>
+              <div className={styles['step-desc']}>
+                <h4>Discover a Trusted Pet Sitter</h4>
+                <p className={styles['mb-0']}>Explore and connect with certified pet sitters in your area for reliable and experienced pet care services.</p>
+              </div>
             </div>
           </Col>
-          <Col md={4}>
-            <div className="text-centre" style={{ textAlign: 'center' }}>
-              <div className={styles['blue-circle']}>
-                <FaCashRegister className={styles['icon']} />
+          <Col lg={4} md={12} className="text-center">
+            <div className={styles['work-process']}>
+              <div className={styles['box-loader']}>
+                <span></span>
+                <span></span>
+                <span></span>
               </div>
-              <h2>Pay online</h2>
-              <p>Book your sitter through our safe online system, with flexible cancellations in case your plans change.</p>
+              <div className={styles['step-num-box']}>
+                <div className={styles['step-icon']}>
+                  <span>
+                    <FiUserCheck />
+                  </span>
+                </div>
+                <div className={styles['step-num']}>02</div>
+              </div>
+              <div className={styles['step-desc']}>
+                <h4>Schedule a Meet & Greet Session</h4>
+                <p className={styles['mb-0']}>Schedule a meet & greet session to personally connect with your sitter, with no strings attached.</p>
+              </div>
+            </div>
+          </Col>
+          <Col lg={4} md={12} className="text-center">
+            <div className={styles['work-process']}>
+              <div className={styles['step-num-box']}>
+                <div className={styles['step-icon']}>
+                  <span>
+                    <BsCreditCard2Back />
+                  </span>
+                </div>
+                <div className={styles['step-num']}>03</div>
+              </div>
+              <div className={styles['step-desc']}>
+                <h4>Secure Online Payments:</h4>
+                <p className={styles['mb-0']}>Reserve Your Sitter with Our Safe Online Platform, Featuring Flexible Cancellation Options.</p>
+              </div>
             </div>
           </Col>
         </Row>
       </Container>
-    </div>
+    </section>
   );
 }
 
 export default HowItWorks;
+
+// import React from 'react';
+// import { Container, Row, Col } from 'react-bootstrap';
+// import { FaSearch, FaCalendar, FaCashRegister } from 'react-icons/fa';
+// import styles from './component.module.scss';
+
+// function HowItWorks() {
+//   return (
+//     <div>
+//       <div>
+//       <h2 className="text-center font-weight-bold">Here's how JOLLY works.</h2>
+//         <p className="text-center">Search and connect with trusted pet sitters. It's easy as 1, 2, 3.</p>
+//       </div>
+//       <Container className='pt-4' style={{ minHeight: '50vh' }}>
+//         <Row className="justify-content-center">
+//           <Col md={4}>
+//             <div className="text-centre" style={{ textAlign: 'center' }}>
+//               <div className={styles['blue-circle']}>
+//                 <FaSearch className={styles['icon']} />
+//               </div>
+//               <h2>Find a verified pet sitter</h2>
+//               <p>Search and find experienced local pet sitters nearby</p>
+//             </div>
+//           </Col>
+//           <Col md={4} className="justify-content-center align-items-center">
+//             <div className="text-centre" style={{ textAlign: 'center' }}>
+//               <div className={styles['blue-circle']}>
+//                 <FaCalendar className={styles['icon']} />
+//               </div>
+//               <h2>Arrange a meet & greet</h2>
+//               <p>Get to know your sitter in person, obligation-free.</p>
+//             </div>
+//           </Col>
+//           <Col md={4}>
+//             <div className="text-centre" style={{ textAlign: 'center' }}>
+//               <div className={styles['blue-circle']}>
+//                 <FaCashRegister className={styles['icon']} />
+//               </div>
+//               <h2>Pay online</h2>
+//               <p>Book your sitter through our safe online system, with flexible cancellations in case your plans change.</p>
+//             </div>
+//           </Col>
+//         </Row>
+//       </Container>
+//     </div>
+//   );
+// }
+
+// export default HowItWorks;
--- frontend/src/components/layout/nav/index.tsx
+++ frontend/src/components/layout/nav/index.tsx
@@ -1,35 +1,31 @@
-"use client"
+'use client';
 import { Button, Container, Navbar, Nav as NavCompo, NavDropdown, Offcanvas } from 'react-bootstrap';
 
 const Nav = () => {
   return (
-    <Navbar key="lag" expand='lg' className="main-page__nav-bar" data-bs-theme="dark" style={{ height: '65px' }} >
-    <Container fluid>
-      <Navbar.Brand href="#">JOLLY</Navbar.Brand>
-      <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
-      <Navbar.Offcanvas
-        id={`offcanvasNavbar-expand-lg`}
-        aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
-        placement="end"
-      >
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
-            Offcanvas
-          </Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          <NavCompo className="justify-content-end flex-grow-1 pe-3">
-            <NavCompo.Item>
-              <Button variant='light' className='main-page__nav-bar__item_button'>Become a sitter</Button>
-            </NavCompo.Item>
-            <NavCompo.Link href="#action2">Sign up</NavCompo.Link>
-            <NavCompo.Link href="#action2">Login</NavCompo.Link>
-            <NavCompo.Link href="#action2">Help</NavCompo.Link>
-          </NavCompo>
-        </Offcanvas.Body>
-      </Navbar.Offcanvas>
-    </Container>
-  </Navbar>
+    <Navbar key="lag" expand="sm" className="main-page__nav-bar" data-bs-theme="dark" style={{ height: '65px' }}>
+      <Container fluid>
+        <Navbar.Brand href="#">JOLLY</Navbar.Brand>
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-sm`} />
+        <Navbar.Offcanvas id={`offcanvasNavbar-expand-sm`} aria-labelledby={`offcanvasNavbarLabel-expand-sm`} placement="end">
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-sm`}>JOLLY</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <NavCompo className="justify-content-end flex-grow-1 pe-3">
+              <NavCompo.Item>
+                <Button variant="light" className="main-page__nav-bar__item_button">
+                  Become a sitter
+                </Button>
+              </NavCompo.Item>
+              <NavCompo.Link href="#action2">Sign up</NavCompo.Link>
+              <NavCompo.Link href="#action2">Login</NavCompo.Link>
+              <NavCompo.Link href="#action2">Help</NavCompo.Link>
+            </NavCompo>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
   );
 };
 
