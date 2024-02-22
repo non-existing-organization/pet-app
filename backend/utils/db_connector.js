815e433fa4812be675fc4b4733d68b7c9c9125a1
John Stilia <stilia.johny@gmail.com>
2024-02-22 21:36:11 +0000

    docs: Update database module documentation

--- backend/utils/db_connector.js
+++ backend/utils/db_connector.js
@@ -1,14 +1,24 @@
-// db.js
+/**
+ * Database module for creating a pool of PostgreSQL connections.
+ * Utilizes environment variables for database configuration.
+ * 
+ * @module db
+ * @requires pg
+ */
 
-const { Pool } = require('pg')
-require('dotenv').config()
+const { Pool } = require('pg');
+require('dotenv').config();
 
+/**
+ * A pool of PostgreSQL connections.
+ * @type {Pool}
+ */
 const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME
-})
+});
 
-module.exports = pool
+module.exports = pool;
